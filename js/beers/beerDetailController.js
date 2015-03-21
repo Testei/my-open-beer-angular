c66b998b19ff819b32390be80b0eccfcfa3596c6
Testei <jasmine50@hotmail.fr>
2015-03-21 22:18:44 +0100

    truc avec erreur
    il y a une erreur que je comprend pas

--- js/beers/beerDetailController.js
+++ js/beers/beerDetailController.js
@@ -1,31 +1,39 @@
-module.exports=function($scope,config,$location,rest,save,$document,modalService) {
-	$scope.data={};
-	$scope.data["beers"]=config.beers.all;
-	var self=this;
-	var selfScope=$scope;
-	$scope.config.activeBeers;
-	$scope.setDetailScope=function(detail){
-	$scope.ViewBeerDetail=detail;
-	};
-	var onRouteChangeOff= $scope.$on('$locationChangeStart', function routeChange(event, newDescription, oldDescription) {
-		if (!$scope.ViewBeerDetail || !$scope.ViewBeerDetail.$dirty || $scope.exit) return;
-
-		var alert = modalService.showModal("Sortie","<b>Attention</b>, si vous continuez, vous perdez les modifications en cours.<br>Enregistrer avant sortie ?",
-				function(value){
-				selfScope.exit=true;
-				if(value=="Enregistrer et continuer"){
-					onRouteChangeOff();
-					if(selfScope._update()==true){
-						$location.path(newDescription.substring($location.absDescription().length - $location.description().length));
-					}
-				}else if(value=="Continuer"){
-					console.log(value);
-					onRouteChangeOff();
-					$location.path(newDescription.substring($location.absDescription().length - $location.description().length));
-				}
+module.exports=function($scope,config,$location,rest,save,$document,modalService, $controller){
+	
+/*
+	if(angular.isUndefined(config.activeBeer)){
+		$location.path("beers/");
+	}
+	$scope.activeBeer=config.activeBeer;
+	
+	$scope._update=function(beer,force,callback){
+		var result=false;
+		if(force || $scope.frmBeer.$dirty){
+			if(angular.isUndefined(beer)){
+				beer=$scope.activeBeer;
+			}else{
+				config.activeBeer=angular.copy(beer);
+				config.activeBeer.reference=beer;
 			}
-		);
-		event.preventDefault();
-		return;
-	});
+			$scope.data.posted={
+			    "name" : beer.name,
+			    "description"  : beer.description
+			};
+			
+			config.activeBeer.reference.name=$scope.activeBeer.name;
+			config.activeBeer.reference.description=$scope.activeBeer.description;
+			config.activeBeer.reference.updated_at=new Date();
+			
+			if(config.beers.update==="immediate" || force)
+				rest.put(config.activeBeer.id,$scope.data,"beers",config.activeBeer.name,callback);
+			else{
+				config.activeBeer.reference.flag="Updated";
+				save.addOperation("Updated",$scope.update,config.activeBeer.reference);
+				result=true;
+			}
+		}else{
+			result=true;
+		}
+		return result;
+	}*/
 };
--- js/beers/beerUpdateController.js
+++ js/beers/beerUpdateController.js
@@ -1,4 +1,42 @@
 module.exports=function($scope,config,$location,rest,save,$document,modalService, $controller){
+/*	$controller('BeerAddController', {$scope: $scope});
+
+	if(angular.isUndefined(config.activeBeer)){
+		$location.path("beers/");
+	}
+	$scope.activeBeer=config.activeBeer;
+	
+	$scope._update=function(beer,force,callback){
+		var result=false;
+		if(force || $scope.frmBeer.$dirty){
+			if(angular.isUndefined(beer)){
+				beer=$scope.activeBeer;
+			}else{
+				config.activeBeer=angular.copy(beer);
+				config.activeBeer.reference=beer;
+			}
+			$scope.data.posted={
+			    "name" : beer.name,
+			    "description"  : beer.description
+			};
+			
+			config.activeBeer.reference.name=$scope.activeBeer.name;
+			config.activeBeer.reference.description=$scope.activeBeer.description;
+			config.activeBeer.reference.updated_at=new Date();
+			
+			if(config.beers.update==="immediate" || force)
+				rest.put(config.activeBeer.id,$scope.data,"beers",config.activeBeer.name,callback);
+			else{
+				config.activeBeer.reference.flag="Updated";
+				save.addOperation("Updated",$scope.update,config.activeBeer.reference);
+				result=true;
+			}
+		}else{
+			result=true;
+		}
+		return result;
+	}
+};module.exports=function($scope,config,$location,rest,save,$document,modalService, $controller){
 	$controller('BeerAddController', {$scope: $scope});
 
 	if(angular.isUndefined(config.activeBeer)){
@@ -17,11 +55,11 @@ module.exports=function($scope,config,$location,rest,save,$document,modalService
 			}
 			$scope.data.posted={
 			    "name" : beer.name,
-			    "url"  : beer.url
+			    "description"  : beer.description
 			};
 			
 			config.activeBeer.reference.name=$scope.activeBeer.name;
-			config.activeBeer.reference.url=$scope.activeBeer.url;
+			config.activeBeer.reference.description=$scope.activeBeer.description;
 			config.activeBeer.reference.updated_at=new Date();
 			
 			if(config.beers.update==="immediate" || force)
@@ -35,5 +73,5 @@ module.exports=function($scope,config,$location,rest,save,$document,modalService
 			result=true;
 		}
 		return result;
-	}
+	}*/
 };
